329adad5a90a84384762b74557226bd8e80393ae
lukjjz95 <lukjjz.dev@gmail.com>
2023-08-22 00:44:30 -0300

    update v2.2.1

---
+++ src/Components/GalleryHome/GalleryHome.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import "./GalleryHome.scss"
+
+
+export const GalleryHome = () =>{
+
+
+    return(
+
+
+        <div>
+            <section className="sectionGalleryHome">
+
+            <div className="galleryCard">
+                    <img src="https://www.singulart.com/images/artworks/v2/cropped/27745/main/zoom/1670203_a57cd2685cf6aa2ba36d783fc2f2c751.jpeg" alt='asd'/>
+                    <p>Texto de la primera card</p>
+                </div>
+                
+                <div className="galleryCard">
+                    <img src="https://www.singulart.com/images-sh/eyJidWNrZXQiOiJzaW5ndWxhcnQtd2Vic2l0ZS1wcm9kIiwia2V5IjoiYXJ0d29ya3NcL3BpY3R1cmVzXC9jcm9wcGVkXC83MTg5XC8yMzg0MVwvem9vbVwvc2VyaWVfMjM4NDFfZTk1MGYwYTRkMTc0OGRjYzI1ZjRmMzc4NmIzZWQ1YzcuanBlZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTA4NCwiaGVpZ2h0Ijo3NzgsImZpdCI6Imluc2lkZSIsImJhY2tncm91bmQiOm51bGx9LCJ0b0Zvcm1hdCI6IndlYnAifX0=?signature=13b8462672f06a9a091cda99f4ab7e3fd524d64d76bcb3c5a135c01d9ad0efe7" alt='asd'/>
+                    <p>Texto de la segunda card</p>
+                </div>
+
+
+                <div className="galleryCard">
+                    <img src="https://www.singulart.com/images/artworks/v2/cropped/27745/main/zoom/1670203_a57cd2685cf6aa2ba36d783fc2f2c751.jpeg" alt='asd'/>
+                    <p>Texto de la primera card</p>
+                </div>
+                
+                <div className="galleryCard">
+                <img src="https://static.abc.es/media/cultura/2020/10/13/david-kdXC-U401363051755OrE-620x849@abc.jpg" alt='asd'/>
+                    <p>Texto de la segunda card</p>
+                </div>
+
+                <div className="galleryCard">
+                <img src="https://www.telefonica.com/es/wp-content/uploads/sites/4/2023/01/criptoarte-blockchain.jpg?w=1200" alt='asd'/>
+                    <p>Texto de la primera card</p>
+                </div>
+                
+                <div className="galleryCard">
+                <img src="https://blogimages.musement.com/2020/07/Pop-artists.jpg" alt='asd'/>
+                    <p>Texto de la segunda card</p>
+                </div>
+                
+
+
+
+
+            </section>
+
+
+        </div>
+
+
+    )
+}
+
+export default GalleryHome;
