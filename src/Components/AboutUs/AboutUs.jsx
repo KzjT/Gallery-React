c60b788a282500da72f01aedc31c01df1e666719
lukjjz95 <lukjjz.dev@gmail.com>
2023-08-26 10:39:39 -0300

    segunda preentrega con add+

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,24 +1,130 @@
-import './AboutUs.scss'
+import React from 'react';
+import './AboutUs.scss';
+import direct1 from "../../img/direct1.jpg";
+import direct2 from "../../img/direct2.jpg";
+import expo1 from "../../img/expo1.jpeg";
+import expo2 from "../../img/expo2.jpg";
+import expo3 from "../../img/expo3.jpg";
 
 
-export const AboutUs = () => {
-
-
+const AboutUs = () => {
     return (
+        <div className="about-us container-fluid">
+            <section className="owners-section container-fluid">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-6">
+                            <div className="owners">
+                                <div className="owner">
+                                    <img src={direct2} alt="Owner 1" className="img-fluid rounded-circle owner-img" />
+                                    <h4 className="owner-name">Ana García</h4>
+                                    <p className="owner-role">Creative director</p>
+                                </div>
+                                <div className="owner">
+                                    <img src={direct1} alt="Owner 2" className="img-fluid rounded-circle owner-img" />
+                                    <h4 className="owner-name">Luis Martínez</h4>
+                                    <p className="owner-role"> Main Artist</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="about-content">
+                                <h2 className="section-title title-color">About Us</h2>
+                                <p className="lead">
+                                    Welcome to our art studio, where creativity and passion come together to create works
+                                    amazing. With years of experience in the world of art, we are proud to present our creations
+                                    unique and expressive.
+                                </p>
+                                <p>
+                                    Our team is made up of Ana García, our Creative Director, and Luis Martínez, our
+                                    Main Artist. Together, we combine our unique visions to produce art that resonates with
+                                    emotions and deep meanings.
+                                </p>
+                                <p>
+                                    Join us on this exciting artistic journey and explore the world of creativity with us.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-        
-        <div>
+            <section className="work-section">
+                <div className="container">
+                    <h2 className="section-title title-color"> Our work</h2>
+                    <p className="lead">
+                        Explore some of our latest artwork and discover the diversity of styles and techniques that
+                        we use to express our creativity.
+                    </p>
+                    <div class="row featurette mt-5">
+                        <div class="col-md-7 ">
+                            <h2 class="featurette-heading fw-normal lh-1 title-color">First featurette heading. <span class="text-body-secondary title-color">It’ll blow your mind.</span></h2>
+                            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+                        </div>
+                        <div class="col-md-5">
+                            <img
+                                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto container-img-aboutUs"
+                                width="500"
+                                height="500"
+                                src={expo1}
+                                alt="Placeholder: 500x500"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                            />
 
+                        </div>
+                    </div>
 
+                    <hr class="featurette-divider mb-5" />
 
-        <h1>About us</h1>
+                    <div class="row featurette">
+                        <div class="col-md-7 order-md-2">
+                            <h2 class="featurette-heading fw-normal lh-1 title-color">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
+                            <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+                        </div>
+                        <div class="col-md-5 order-md-1">
+                            <img
+                                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto container-img-aboutUs"
+                                width="500"
+                                height="500"
+                                src={expo3}
+                                alt="Placeholder: 500x500"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                            />
 
-        <hr/>
+                        </div>
+                    </div>
 
-        
-            
+                    <hr class="featurette-divider mb-5" />
+
+                    <div class="row featurette">
+                        <div class="col-md-7">
+                            <h2 class="featurette-heading fw-normal lh-1 title-color">And lastly, this one. <span class="text-body-secondary">Checkmate.</span></h2>
+                            <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+                        </div>
+                        <div class="col-md-5">
+                            <img
+                                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto container-img-aboutUs"
+                                width="500"
+                                height="500"
+                                src={expo2}
+                                alt="Placeholder: 500x500"
+                                aria-label="Placeholder: 500x500"
+                                preserveAspectRatio="xMidYMid slice"
+                                focusable="false"
+                            />
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <hr class="featurette-divider mb-0" />
         </div>
-    )
-}
+    );
+};
 
 export default AboutUs;
