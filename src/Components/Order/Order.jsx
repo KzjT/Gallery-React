897ed640dd54d1771be3c4a94e2403328c035f1a
lukjjz95 <lukjjz.dev@gmail.com>
2023-09-17 22:11:44 -0300

    asd

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -12,40 +12,40 @@ const Order = ({ id, items, total }) => {
 
     return (
         <div className="order-details">
-          <div className="order-content">
-            <h3 className="order-header">Order Details</h3>
-            <div className="order-info">
-              <p className="order-id">Order #{id}</p>
-              <p className="order-date">Fecha de compra: {formattedDateTime}</p>
-            </div>
-            <div className="order-items">
-              <h4 className="items-header">Order Items:</h4>
-              <ul className="item-list">
-                {items.map((item, index) => (
-                  <li key={item.id} className="item">
-                    <div className="product-info">
-                      <img src={item.img} alt={item.name} className="product-image" />
-                      <div className="product-details">
-                        <p className="product-name">{item.name}</p>
-                        <p className="item-quantity">×{item.quantity}</p>
-                        <p className="item-price">{formatter2.format(item.price * item.quantity)}</p>
-                      </div>
+            <div className="order-content">
+                <h3 className="order-header">Order Details</h3>
+                <div className="order-info">
+                    <p className="order-id">Order #{id}</p>
+                    <p className="order-date">Fecha de compra: {formattedDateTime}</p>
+                </div>
+                <div className="order-items">
+                    <h4 className="items-header">Order Items:</h4>
+                    <ul className="item-list">
+                        {items.map((item, index) => (
+                            <li key={item.id} className="item">
+                                <div className="product-info">
+                                    <img src={item.img} alt={item.name} className="product-image" />
+                                    <div className="product-details">
+                                        <p className="product-name">{item.name}</p>
+                                        <p className="item-quantity">×{item.quantity}</p>
+                                        <p className="item-price">{formatter2.format(item.price * item.quantity)}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="order-summary">
+                        <p>Subtotal: {formatter2.format(total)}</p>
+                        <p>Envío: $0</p>
+                        <p className='total-order'>Total: {formatter2.format(total)}</p>
                     </div>
-                  </li>
-                ))}
-              </ul>
-              <div className="order-summary">
-                <p>Subtotal: {formatter2.format(total)}</p>
-                <p>Envío: $0</p>
-                <p className='total-order'>Total: {formatter2.format(total)}</p>
-              </div>
+                </div>
             </div>
-          </div>
-          <div className="order-actions">
-            <Link to="/" onClick={clear} className="btn btn-primary btn-order">Home</Link>
-          </div>
+            <div className="order-actions">
+                <Link to="/" onClick={clear} className="btn btn-primary btn-order">Home</Link>
+            </div>
         </div>
-      );
-    };
-    
-    export default Order;
+    );
+};
+
+export default Order;
